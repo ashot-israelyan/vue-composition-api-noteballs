01420d3fb8491e99b6d988f54e4aa513806f8812
Ashot Israelyan <ashot.israelyan@tumo.org>
2024-04-17 16:02:59 +0400

    initial commit

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
+import ViewNotes from '@/views/ViewNotes.vue'
+import ViewStats from '@/views/ViewStats.vue'
+import ViewEditNote from '@/views/ViewEditNote.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'notes',
+    component: ViewNotes
+  },
+  {
+    path: '/editNote/:id',
+    name: 'edit-note',
+    component: ViewEditNote
+  },
+  {
+    path: '/stats',
+    name: 'stats',
+    component: ViewStats
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="notes">
+
+    <AddEditNote
+      v-model="newNote"
+      placeholder="Add a new note"
+      ref="addEditNoteRef"
+    >
+      <template #buttons>
+        <button
+          @click="addNote"
+          :disabled="!newNote"
+          class="button is-link has-background-success"
+        >
+          Add New Note
+        </button>
+      </template>
+    </AddEditNote>
+
+    <Note
+      v-for="note in storeNotes.notes"
+      :key="note.id"
+      :note="note"
+    />
+
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  import Note from '@/components/Notes/Note.vue'
+  import AddEditNote from '@/components/Notes/AddEditNote.vue'
+  import { useStoreNotes } from '@/store/storeNotes'
+
+  const storeNotes = useStoreNotes()
+
+  const newNote = ref('')
+  const addEditNoteRef = ref(null)
+
+  const addNote = () => {
+    storeNotes.addNote(newNote.value)
+    newNote.value = ''
+    addEditNoteRef.value.focusTextarea()
+  }
+
+</script>
